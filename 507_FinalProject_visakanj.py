471e4a7362248a8fe53f5f44d12379d71bc6a394
Visakan Jayakumar <visakanj@visakans-mbp.attlocal.net>
2018-04-11 16:02:20 -0400

    Google requesting

--- 507_FinalProject_visakanj.py
+++ 507_FinalProject_visakanj.py
@@ -3,6 +3,8 @@ import requests
 import secret_data # file that contains OAuth credentials
 import json
 import webbrowser
+from bs4 import BeautifulSoup
+
 
 CLIENT_ID = secret_data.CLIENTid
 CLIENT_SECRET = secret_data.CLIENTsecret
@@ -16,35 +18,106 @@ TOKEN_URL = "https://accounts.spotify.com/api/token"
 # CACHE
 CACHE_FNAME = "spotify_cache.json"
 try:
-    cache_file = open(CACHE_FNAME, 'r')
-    cache_contents = cache_file.read()
-    CACHE_DICTION = json.loads(cache_contents)
-    cache_file.close()
+	cache_file = open(CACHE_FNAME, 'r')
+	cache_contents = cache_file.read()
+	CACHE_DICTION = json.loads(cache_contents)
+	cache_file.close()
 
 except:
-    CACHE_DICTION = {}
+	CACHE_DICTION = {}
+
+CACHE_GOOGLE = "google_cache.json"
+try:
+	google_cache_file = open(CACHE_GOOGLE, 'r')
+	google_cache_contents = google_cache_file.read()
+	CACHE_DICTION_GOOGLE = json.loads(google_cache_contents)
+	google_cache_file.close()
+
+except:
+	CACHE_DICTION_GOOGLE = {}
+
+# Take user input(s) >> form params dict >> make unique url >> request Google search >> scrape
+
+def form_google_params(search_terms):
+	# search_terms = [ ]
+
+	# return params dict
+	pass
 
 def params_unique_combination(baseurl, params):
-    alphabetized_keys = sorted(params.keys())
-    res = []
-    for k in alphabetized_keys:
-        res.append("{}={}".format(k, params[k]))
-    return baseurl + "&".join(res)
+	alphabetized_keys = sorted(params.keys())
+	res = []
+	for k in alphabetized_keys:
+		res.append("{}={}".format(k, params[k]))
+	return baseurl + "&".join(res)
 
-# def call_spotify():
-# 	oauth2inst = requests_oauthlib.OAuth2Session(CLIENT_ID, redirect_uri=redirect_URI)
-# 	auth_url, state = oauth2inst.authorization_url(authorization_url)
+def google_make_request_and_cache(baseurl, params):
+	global header
+	header = {'User-Agent': 'Google_Project'}
 
-# 	webbrowser.open(auth_url)
-# 	auth_response = input('Confirm authentication and enter the full URL').strip()
+	unique_ident = params_unique_combination(baseurl, params)
 
-# 	auth_token = oauth2inst.fetch_token(TOKEN_URL, authorization_response = auth_response, client_secret = CLIENT_SECRET)
+	if unique_ident in CACHE_DICTION_GOOGLE:
+		print("Getting cached data...")
+		return CACHE_DICTION_GOOGLE[unique_ident]
 
-# 	r = oauth2inst.get('https://api.spotify.com/v1/search?q=Kanye&type=track&limit=5')
-# 	response_dict = json.loads(r.text)
-# 	print(json.dumps(response_dict, indent = 2))
+	else:
+		print("Making a request for new data...")
+		resp = requests.get(unique_ident, headers=header)
+		CACHE_DICTION_GOOGLE[unique_ident] = resp.text
 
-def make_request_and_cache(baseurl, params):
+		dumped_json_cache = json.dumps(CACHE_DICTION_GOOGLE)
+		fw = open(CACHE_GOOGLE,"w")
+		fw.write(dumped_json_cache)
+		fw.close()
+		return CACHE_DICTION_GOOGLE[unique_ident]
+
+def get_google_data(baseurl, params):
+	google_descriptions_dict = {}
+	page_count = -19
+	links = []
+	next_links = []
+	descriptions_list = []
+	
+	# make this a function
+	google_page = google_make_request_and_cache(baseurl, params)
+
+	page_soup = BeautifulSoup(google_page, 'html.parser')
+	content = page_soup.find_all(class_='r')
+	
+	for link in content:
+		link_title = link.text
+		if "Images" not in link_title:
+			links.append(link_title)
+
+	descriptions = page_soup.find_all(class_ = 'st')
+
+	for description in descriptions:
+		descriptions_list.append(description.text)
+	# make function through here
+
+	next_link_section = page_soup.find_all('a')
+	while page_count < -10:
+		next_link = (next_link_section)[page_count].get('href')
+		next_links.append(next_link)
+		page_count += 1
+
+	# Request for each link in next_links
+
+	n = 0
+	for link in links:
+		google_descriptions_dict[link] = descriptions_list[n]
+		n += 1
+	
+	print((links))
+	print(len(descriptions_list))
+	print(len(google_descriptions_dict))
+
+	print(google_descriptions_dict.keys())
+	return []
+	
+
+def spotify_make_request_and_cache(baseurl, params):
 	unique_ident = params_unique_combination(baseurl, params)
 
 	if unique_ident in CACHE_DICTION:
@@ -75,9 +148,10 @@ def make_request_and_cache(baseurl, params):
 		return CACHE_DICTION[unique_ident]
 
 spotify_baseurl = 'https://api.spotify.com/v1/search?'
-params_dict = {'q':'Welcome', 'type':'track', 'limit':5}
-make_request_and_cache(spotify_baseurl, params_dict)
+spotify_params_dict = {'q':'Happy', 'type':'track', 'limit':10}
+# spotify_make_request_and_cache(spotify_baseurl, spotify_params_dict)
 
-
-
+google_baseurl = 'https://www.google.com/search?'
+google_params_dict = {'q':'welcome+happy+help'}
+get_google_data(google_baseurl, google_params_dict)
 
